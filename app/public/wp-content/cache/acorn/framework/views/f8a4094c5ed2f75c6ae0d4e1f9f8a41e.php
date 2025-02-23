53cb05cc98118035720142f1a6516f90209be1dc
ericktomaliwan <ericktoma@outlook.com>
2025-02-23 17:46:33 +1100

    initial footer

---
+++ app/public/wp-content/cache/acorn/framework/views/f8a4094c5ed2f75c6ae0d4e1f9f8a41e.php
@@ -0,0 +1,9 @@
+<?php (the_content()); ?>
+
+<?php if($pagination): ?>
+  <nav class="page-nav" aria-label="Page">
+    <?php echo $pagination; ?>
+
+  </nav>
+<?php endif; ?>
+<?php /**PATH /Users/ericktomaliwan/Local Sites/rhino/app/public/wp-content/themes/rhinosports/resources/views/partials/content-page.blade.php ENDPATH**/ ?>
